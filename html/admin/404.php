f296b5879cb37a5b1bd44c71a14d34376908f96e
Iris88zjm <327055610@qq.com>
2017-11-14 17:10:25 +0800

    add 404 page

---
+++ html/admin/404.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<title>404</title>
+    <link rel="stylesheet" type="text/css" href="//at.alicdn.com/t/font_470938_k9amf1s1c06647vi.css">
+	<link rel="stylesheet" type="text/css" href="css/login.css">
+</head>
+<body>
+	<div class="nofound">		
+		<div class="content">			
+			<div class="title">404</div>
+			<div class="text">
+				<p>对不起！您找的页面不存在</p>
+				<a href="index.php" class="btn btn-back">回到首页</a>
+			</div>
+		</div>
+	</div>
+</body>
+</html>
